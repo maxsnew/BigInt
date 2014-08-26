8cd496af6500938f7c3425d53d6aee139b00477f
Max New <maxsnew@gmail.com>
2014-08-26 18:03:41 -0500

    Add skeleton of test suite and one failing test

---
+++ Test.elm
@@ -0,0 +1,35 @@
+module Main where
+
+import BigInt as I
+
+import ElmTest.Runner.Console (runDisplay)
+import ElmTest.Test (..)
+
+tests = suite "BigInt tests" [
+          suite "From String tests" fromStringTests
+        , suite "To String tests" toStringTests
+        , suite "Addition tests" additionTests
+        , suite "Subtraction tests" subtractionTests
+        , suite "Multiplication tests" multiplicationTests
+        , suite "Division tests" divisionTests
+        , suite "Comparison tests" comparisonTests
+        , suite "Square Root tests" squareRootTests
+        ]
+
+fromStringTests = []
+
+toStringTests = []
+
+additionTests = [I.fromString "101" `equals` (I.add (I.fromString "100") I.one)]
+
+subtractionTests = []
+
+multiplicationTests = []
+
+divisionTests = []
+
+comparisonTests = []
+
+squareRootTests = []
+
+console = runDisplay tests
