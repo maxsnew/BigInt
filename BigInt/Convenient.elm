f92f5a0b527069415e516528df8cc25adf934faf
Max New <maxsnew@gmail.com>
2014-08-27 20:36:13 -0500

    Add convenience module

---
+++ BigInt/Convenient.elm
@@ -0,0 +1,14 @@
+module BigInt.Convenient where
+
+import BigInt as I
+
+zero = I.zero
+one  = I.one
+two  = I.fromString "2"
+three  = I.fromString "3"
+four  = I.fromString "4"
+five  = I.fromString "5"
+six  = I.fromString "6"
+seven  = I.fromString "7"
+eight  = I.fromString "8"
+nine  = I.fromString "9"
